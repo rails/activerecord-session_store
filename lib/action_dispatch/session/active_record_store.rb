a4f3669ab419bd084674953b8b9bed1e01ad0901
pedrozath <pedrozath@gmail.com>
2013-07-31 20:52:08 -0300

    added record.session_id= sid

--- lib/action_dispatch/session/active_record_store.rb
+++ lib/action_dispatch/session/active_record_store.rb
@@ -77,6 +77,7 @@ module ActionDispatch
           ActiveRecord::Base.logger.quietly do
             record = get_session_model(env, sid)
             record.data = session_data
+            record.session_id= sid
             return false unless record.save
 
             session_data = record.data
