06f836ea447191e89aeff76086ae794acbfc8586
Rafael Mendonça França <rafael@rubyonrails.org>
2026-07-13 19:51:16 +0000

    Remove deprecator now that we don't have anything to deprecate

--- lib/active_record/session_store/railtie.rb
+++ lib/active_record/session_store/railtie.rb
@@ -4,10 +4,6 @@ module ActiveRecord
   module SessionStore
     class Railtie < Rails::Railtie
       rake_tasks { load File.expand_path("../../../tasks/database.rake", __FILE__) }
-
-      initializer "activerecord-session_store.deprecator" do |app|
-        app.deprecators[:"activerecord-session_store"] = SessionStore.deprecator
-      end
     end
   end
 end
